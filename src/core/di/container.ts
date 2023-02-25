8ce50fdebff7b076ce364926b2c03c3e06764fab
Andrés Fuentes <andresfuentesfernandez@gmail.com>
2023-02-25 15:54:06 +0100

    feat: adds topic infrastructure

--- src/core/di/container.ts
+++ src/core/di/container.ts
@@ -1,7 +1,12 @@
 import { container } from 'tsyringe'
 import { SpeakerMockRepository } from '../../features/speakers/infrastructure/speaker-mock-repository'
+import { TopicMockRepository } from '../../features/topics/infrastructure/topic-mock-repository'
 import { TOKENS } from './injection-tokens'
 
 container.register(TOKENS.SPEAKER_REPOSITORY, {
     useClass: SpeakerMockRepository
+})
+
+container.register(TOKENS.TOPIC_REPOSITORY, {
+    useClass: TopicMockRepository
 })
