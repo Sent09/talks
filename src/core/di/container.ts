0b80e4804f7f20a72772226651233eba77345732
Andrés Fuentes <andresfuentesfernandez@gmail.com>
2023-02-25 11:39:11 +0100

    fix: moves files to the correct folder

--- src/core/di/container.ts
+++ src/core/di/container.ts
@@ -1,5 +1,5 @@
 import { container } from 'tsyringe'
-import { SpeakerMockRepository } from '../../features/infrastructure/speaker-mock-repository'
+import { SpeakerMockRepository } from '../../features/speakers/infrastructure/speaker-mock-repository'
 import { SPEAKER_REPOSITORY } from './injection-tokens'
 
 container.registerSingleton(SPEAKER_REPOSITORY, SpeakerMockRepository)
