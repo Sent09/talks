39e790c1e45ed5692f006625d1bbc200f6cce5de
Andrés Fuentes <andresfuentesfernandez@gmail.com>
2023-02-27 16:50:49 +0100

    test: changes title

--- src/features/talks/ui/utils/get-hours-duration.spec.ts
+++ src/features/talks/ui/utils/get-hours-duration.spec.ts
@@ -1,7 +1,7 @@
 import { getHoursDuration } from './get-hours-duration';
 
 describe('getHoursDuration', () => {
-    it('should the difference between 2 hours', () => {
+    it('should show the difference between 2 hours', () => {
         const startHour = "10:00"
         const endHour = "11:00"
         const duration = getHoursDuration(startHour, endHour)
