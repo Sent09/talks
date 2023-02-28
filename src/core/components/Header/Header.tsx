87cb585ddebe7b45af531bf235af9726c3ad588f
Andrés Fuentes <andresfuentesfernandez@gmail.com>
2023-02-28 18:17:37 +0100

    feat: menu responsive

--- src/core/components/Header/Header.tsx
+++ src/core/components/Header/Header.tsx
@@ -1,14 +1,17 @@
+import { useState } from 'react'
 import logo from './assets/logo-talks.png'
 import './styles.scss'
 
 export const Header: React.FC = () => {
+    const [showNav, setShowNav] = useState<boolean>(false)
+
     return (
         <header>
             <div className="header-container">
                 <a href="/" className="logo">
                     <img src={logo} alt="Logo de talks" />
                 </a>
-                <nav>
+                <nav className={showNav ? 'show' : ''}>
                     <ul>
                         <li>
                             <a href="/">Inicio</a>
@@ -29,6 +32,11 @@ export const Header: React.FC = () => {
                         </li>
                     </ul>
                 </nav>
+                <button type="button" onClick={() => setShowNav(!showNav)}>
+                    <span className="top"></span>
+                    <span className="mid"></span>
+                    <span className="bottom"></span>
+                </button>
             </div>
         </header>
     )
